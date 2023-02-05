0bc5fadb703fd18a826c157705103c2f30a00c77
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-05 22:13:02 +0200

    added data classes

---
+++ src/modules/tables_classes.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+     name:str
+     title:str
+
+@dataclass
+class Database(Node):
+     pass
+
+class Table(Node):
+     pass
+
+@dataclass
+class DataType:
+     name:str
+
+@dataclass
+class Column(Node):
+     dtype: DataType
+
+class Integer(DataType):
+     pass
+
+class String(DataType):
+     pass

f22a5f7520e6b88567db388d4d546b3589aa4a35
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-05 23:17:14 +0200

    writing load functions for all data classes

---
+++ src/modules/load_data_into_classes.py
@@ -0,0 +1,25 @@
+from multipledispatch import dispatch
+from tables_classes import Node, Database, Column, Table, Integer, String
+
+@dispatch(Node, list)
+def load(class_instance, row):
+    title = row[4].strip()
+    class_instance.title = title
+
+
+@dispatch(Database, list)
+def load(class_instance, row):
+    load(class_instance,row)
+
+@dispatch(Table, list)
+def load(class_instance, row):
+    load(class_instance,row)
+
+@dispatch(Column, list)
+def load(class_instance, row):
+    load(class_instance,row)
+    dtype_title = row[3].strip()
+    if dtype_title == 'integer':
+        class_instance.dtype = Integer()
+    elif dtype_title == 'string':
+        class_instance.dtype = String()

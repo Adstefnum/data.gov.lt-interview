1c82c0cefce6c8e3f27e2154317cf434aed2f0c3
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-07 00:34:37 +0200

    more descriptive name for load function

--- src/modules/load_data_into_classes.py
+++ src/modules/load_data_into_classes.py
@@ -2,22 +2,22 @@ from multipledispatch import dispatch
 from tables_classes import Node, Database, Column, Table, Integer, String
 
 @dispatch(Node, list)
-def load(class_instance, row):
+def load_data(class_instance, row):
     title = row[4].strip()
     class_instance.title = title
 
 
 @dispatch(Database, list)
-def load(class_instance, row):
-    load(class_instance,row)
+def load_data(class_instance, row):
+    load_data(class_instance,row)
 
 @dispatch(Table, list)
-def load(class_instance, row):
-    load(class_instance,row)
+def load_data(class_instance, row):
+    load_data(class_instance,row)
 
 @dispatch(Column, list)
-def load(class_instance, row):
-    load(class_instance,row)
+def load_data(class_instance, row):
+    load_data(class_instance,row)
     dtype_title = row[3].strip()
     if dtype_title == 'integer':
         class_instance.dtype = Integer()

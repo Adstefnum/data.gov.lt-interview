d5f6bedf8ea9cb5467fda056fda2b624ab7c6922
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-07 00:35:56 +0200

    adding a dummy function to return dummy data

---
+++ src/modules/retrieve_data.py
@@ -0,0 +1,20 @@
+table_data = [   
+    
+    ['DB', '', '', '', 'DB 1'],
+    ['', 'TBL', '', '', 'TBL 1'],
+    ['', '', 'COL1', 'int', 'Column 1'],
+    ['', '', 'COL2', 'string', 'Column 2'],
+    ['DB', '', '', '', 'DB 2'],
+    ['', 'TBL', '', '', 'TBL 2'],
+    ['', '', 'COL1', 'int', 'Column 1'],
+    ['', '', 'COL2', 'string', 'Column 2'],
+    ['', 'TBL', '', '', 'TBL 3'],
+    ['', '', 'COL1', 'int', 'Column 1'],
+    ['', '', 'COL2', 'string', 'Column 2'],
+
+]
+
+def get_data():
+    return table_data
+
+#TODO:handle retrieving data in flat format and relationship format

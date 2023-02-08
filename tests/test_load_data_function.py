446827496055c35dfef1829bc98b68f8b262a446
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-08 21:37:44 +0200

    added full import

--- tests/test_load_data_function.py
+++ tests/test_load_data_function.py
@@ -1,5 +1,5 @@
 import pytest
-from modules.tables_classes import Database, Table, Column, Integer, String
+from src.modules.tables_classes import Database, Table, Column, Integer, String
 from src.modules.load_data_into_classes import load_data, load_node_data
 
 def test_load_node_data():

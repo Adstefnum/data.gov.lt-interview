4d888e6349aa9786fc2f8854afe5c61aabc4deb6
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-08 13:18:18 +0200

    fix recursion errors

--- src/modules/load_data_into_classes.py
+++ src/modules/load_data_into_classes.py
@@ -1,25 +1,25 @@
 from multipledispatch import dispatch
 from tables_classes import Node, Database, Column, Table, Integer, String
 
-@dispatch(Node, list)
-def load_data(class_instance, row):
+def load_node_data(class_instance, row):
     title = row[4].strip()
     class_instance.title = title
+    return class_instance
 
 
 @dispatch(Database, list)
 def load_data(class_instance, row):
-    load_data(class_instance,row)
+    return load_node_data(class_instance,row)
 
 @dispatch(Table, list)
 def load_data(class_instance, row):
-    load_data(class_instance,row)
+   return load_node_data(class_instance,row)
 
 @dispatch(Column, list)
 def load_data(class_instance, row):
-    load_data(class_instance,row)
-    dtype_title = row[3].strip()
-    if dtype_title == 'integer':
-        class_instance.dtype = Integer()
-    elif dtype_title == 'string':
-        class_instance.dtype = String()
+    dtype_name = row[3].strip()
+    if dtype_name == 'int':
+        class_instance.dtype = Integer('integer')
+    elif dtype_name == 'string':
+        class_instance.dtype = String('string')
+    return load_node_data(class_instance,row)

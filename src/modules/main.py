772c54c7a93a2b323c9bff27100c626dea84543e
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-08 13:18:29 +0200

    main method to read data

--- src/modules/main.py
+++ src/modules/main.py
@@ -4,34 +4,42 @@ from tables_classes import Node, Database, Column, Table, Integer, String
 
 def run(table_data):
 
-    databases = []
-    tables = []
-    columns = []
-
+#[
+# [DB, [table, [columns]], [table,[columns]]]
+# [DB, [table, [columns]], [table,[columns]]]
+# ]
+    data = []
+    database_count, table_count= -1,0
     for row in table_data:
-        if row[0].strip() and not row[1].strip() and not row[2].strip():
-            databases.append(Database(name="DB", title=row[4].strip()))
-        elif row[0].strip() and row[1].strip() and not row[2].strip():
-            tables.append(Table(name="TBL", title=row[4].strip()))
-        elif row[0].strip() and row[1].strip() and row[2].strip():
-            column = Column(name=row[2].strip(), title=row[4].strip())
-            load_data(column, row)
-            columns.append(column)
+        database_name = row[0].strip()
+        table_name = row[1].strip()
+        column_name = row[2].strip()
+        
+        if database_name:
+            database = Database(name=database_name, title='')
+            fully_loaded_database = load_data(database, row)
+            data.append([fully_loaded_database])
+            database_count += 1
+            table_count = 0
+            
+        elif table_name:
+            table = Table(name=table_name, title='')
+            fully_loaded_table = load_data(table, row)
+            data[database_count].append([fully_loaded_table, []]) 
+            table_count += 1
 
-    for database in databases:
-        load_data(database, None)
-
-    for table in tables:
-        load_data(table, None)
-
-    for column in columns:
-        load_data(column, None)
+        elif column_name:
+            column = Column(name=column_name, title='', dtype=None)
+            fully_loaded_column = load_data(column, row)
+            data[database_count][table_count][1].append(fully_loaded_column)
+        
 
 
 
+    print(data)
 
+        
 
-
-if "__name__" == __main__:
+if __name__ == "__main__":
     table_data = get_data()
     run(table_data)

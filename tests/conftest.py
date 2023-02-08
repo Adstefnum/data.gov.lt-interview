6af21ae13e93ab8776279c7d9cd3ac5cfa4b1eb2
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-08 21:17:00 +0200

    setting up test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,24 @@
 import pytest
 
-@pytest.fixture(scope="session")
-def db_conn():
-    db = ...
-    url = ...
-    with db.connect(url) as conn:  # connection will be torn down after all tests finish
-        yield conn
+
+@pytest.fixture
+def get_data(request):
+    if request.param == 'small':
+        return [
+            ['DB', '', '', '', 'DB 1'],
+            ['', 'TBL', '', '', 'TBL 1'],
+            ['', '', 'COL1', 'int', 'Column 1'],
+        ]
+    elif request.param == 'large':
+        return [
+            ['DB', '', '', '', 'DB 1'],
+            ['', 'TBL', '', '', 'TBL 1'],
+            ['', '', 'COL1', 'int', 'Column 1'],
+            ['DB', '', '', '', 'DB 2'],
+            ['', 'TBL', '', '', 'TBL 2'],
+            ['', '', 'COL1', 'int', 'Column 1'],
+            ['', '', 'COL2', 'string', 'Column 2'],
+            ['', 'TBL', '', '', 'TBL 3'],
+            ['', '', 'COL1', 'int', 'Column 1'],
+            ['', '', 'COL2', 'string', 'Column 2'],
+        ]

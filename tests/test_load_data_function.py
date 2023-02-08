4d08899ec63ee09b6ab981c398335e23f0d05f46
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-08 21:56:27 +0200

    adding needed constructor arguments

--- tests/test_load_data_function.py
+++ tests/test_load_data_function.py
@@ -6,28 +6,28 @@ def test_load_node_data():
     pass
 
 def test_load_data_for_database():
-    database = Database()
     row = ['DB', '', '', '', 'DB 1']
+    database = Database(name=row[1],title='')
     database = load_data(database, row)
     assert database.title == 'DB 1'
 
    
 def test_load_data_for_table():
-    table = Table()
     row = ['', 'TBL', '', '', 'TBL 1']
+    table = Table(name=row[2],title='')
     table = load_data(table, row)
     assert table.title == 'TBL 1'
 
 def test_load_data_for_column_with_integer_datatype():
-    column = Column()
     row = ['', '', 'COL1', 'int', 'Column 1']
+    column = Column(name=row[3],title='',dtype=None)
     column = load_data(column, row)
     assert column.title == 'Column 1'
     assert isinstance(column.dtype, Integer)
 
 def test_load_data_for_column_with_string_datatype():
-    column = Column()
     row = ['', '', 'COL1', 'string', 'Column 2']
+    column = Column(name=row[3],title='',dtype=None)
     column = load_data(column, row)
     assert column.title == 'Column 2'
     assert isinstance(column.dtype, String)

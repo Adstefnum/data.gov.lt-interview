5f172fd82348e870e063d4cf4418c05b5a069f5f
Stephen Adesina <bauerstevenson@gmail.com>
2023-02-09 02:02:57 +0200

    fixing imports

--- src/modules/main.py
+++ src/modules/main.py
@@ -1,5 +1,5 @@
-from read_and_print_data_utils import read_data_into_printable_format, pretty_print_data
-from retrieve_data import get_data
+from modules.read_and_print_data_utils import read_data_into_printable_format, pretty_print_data
+from modules.retrieve_data import get_data
 
 if __name__ == "__main__":
     table_data = get_data()
